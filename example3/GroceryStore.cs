4e1f0342f9cbb28af21faebff6e98c4c4e5fb50a
Victor Lu <75279620+victlu@users.noreply.github.com>
2021-02-10 11:19:32 -0800

    Update output of Grocery example

--- example3/GroceryStore.cs
+++ example3/GroceryStore.cs
@@ -91,30 +91,56 @@ namespace Example
 
         /*
 *** Collect...
-Counter/StoreMetrics/item_counter/_Total
-  CountSumMinMax: n=5, sum=18, min=1, max=10
-Counter/StoreMetrics/item_counter/Store=Portland
-  CountSumMinMax: n=5, sum=18, min=1, max=10
-Counter/StoreMetrics/item_counter/Item=potato
-  CountSumMinMax: n=2, sum=4, min=2, max=2
-Counter/StoreMetrics/item_counter/Customer=customerA
-  CountSumMinMax: n=3, sum=6, min=1, max=3
-Counter/StoreMetrics/item_counter/Item=tomato
-  CountSumMinMax: n=3, sum=14, min=1, max=10
-Counter/StoreMetrics/cash_counter/_Total
-  CountSumMinMax: n=4, sum=46.400000000000006, min=2.2, max=30
-Counter/StoreMetrics/cash_counter/Store=Portland
-  CountSumMinMax: n=4, sum=46.400000000000006, min=2.2, max=30
-Counter/StoreMetrics/cash_counter/Customer=customerA
-  CountSumMinMax: n=2, sum=14.2, min=3, max=11.2
-Counter/StoreMetrics/item_counter/Customer=customerB
-  CountSumMinMax: n=1, sum=10, min=10, max=10
-Counter/StoreMetrics/cash_counter/Customer=customerB
-  CountSumMinMax: n=1, sum=30, min=30, max=30
-Counter/StoreMetrics/item_counter/Customer=customerC
-  CountSumMinMax: n=1, sum=2, min=2, max=2
-Counter/StoreMetrics/cash_counter/Customer=customerC
-  CountSumMinMax: n=1, sum=2.2, min=2.2, max=2.2
+Counter/StoreMetrics/cash_counter/CountSumMinMax/_Total
+    CountSumMinMax: n=4, sum=46.400000000000006, min=2.2, max=30
+
+Counter/StoreMetrics/cash_counter/CountSumMinMax/Customer=CustomerA
+    CountSumMinMax: n=4, sum=28.4, min=3, max=11.2
+
+Counter/StoreMetrics/cash_counter/CountSumMinMax/Customer=CustomerB
+    CountSumMinMax: n=1, sum=30, min=30, max=30
+
+Counter/StoreMetrics/cash_counter/CountSumMinMax/Customer=CustomerC
+    CountSumMinMax: n=2, sum=4.4, min=2.2, max=2.2
+
+Counter/StoreMetrics/cash_counter/CountSumMinMax/Store=Portland
+    CountSumMinMax: n=4, sum=46.400000000000006, min=2.2, max=30
+
+Counter/StoreMetrics/cash_counter/LabelHistogram/_Total
+    LabelHistogram: _total=4, Store:Portland=4, Customer:CustomerA=2, Customer:CustomerB=1, Customer:CustomerC=1
+
+Counter/StoreMetrics/item_counter/CountSumMinMax/_Total
+    CountSumMinMax: n=5, sum=18, min=1, max=10
+
+Counter/StoreMetrics/item_counter/CountSumMinMax/Customer=CustomerA
+    CountSumMinMax: n=3, sum=6, min=1, max=3
+
+Counter/StoreMetrics/item_counter/CountSumMinMax/Customer=CustomerA/Item=potato
+    CountSumMinMax: n=1, sum=2, min=2, max=2
+
+Counter/StoreMetrics/item_counter/CountSumMinMax/Customer=CustomerA/Item=tomato
+    CountSumMinMax: n=2, sum=4, min=1, max=3
+
+Counter/StoreMetrics/item_counter/CountSumMinMax/Customer=CustomerB
+    CountSumMinMax: n=1, sum=10, min=10, max=10
+
+Counter/StoreMetrics/item_counter/CountSumMinMax/Customer=CustomerC
+    CountSumMinMax: n=1, sum=2, min=2, max=2
+
+Counter/StoreMetrics/item_counter/CountSumMinMax/Customer=CustomerC/Item=potato
+    CountSumMinMax: n=1, sum=2, min=2, max=2
+
+Counter/StoreMetrics/item_counter/CountSumMinMax/Item=potato/Store=Portland
+    CountSumMinMax: n=2, sum=4, min=2, max=2
+
+Counter/StoreMetrics/item_counter/CountSumMinMax/Item=tomato
+    CountSumMinMax: n=4, sum=24, min=1, max=10
+
+Counter/StoreMetrics/item_counter/CountSumMinMax/Item=tomato/Store=Portland
+    CountSumMinMax: n=3, sum=14, min=1, max=10
+
+Counter/StoreMetrics/item_counter/LabelHistogram/_Total
+    LabelHistogram: _total=5, Store:Portland=5, Item:potato=2, Customer:CustomerA=3, Item:tomato=3, Customer:CustomerB=1, Customer:CustomerC=1
         */
     }
 }
